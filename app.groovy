5f44863e47531ee3012d04024249a4107b9827ff
Greg Turnquist <gturnquist@gopivotal.com>
2013-10-28 11:55:31 -0500

    Doesn't work

--- app.groovy
+++ app.groovy
@@ -80,3 +80,6 @@ class IssueAggregator implements CommandLineRunner {
 		}
 	}
 }
+
+
+
--- web.groovy
+++ web.groovy
@@ -16,6 +16,7 @@
 
 package io.spring.guideissues
 
+@Grab("org.springframework.boot:spring-boot-starter-web:0.5.0.M5")
 @Grab("org.thymeleaf:thymeleaf-spring3:2.0.17")
 
 @Controller
