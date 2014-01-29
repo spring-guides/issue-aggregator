217718c66d3df79372b3ba0ef9b1da6f4b6eeb11
Greg Turnquist <gturnquist@gopivotal.com>
2014-01-29 15:58:13 -0600

    Update so it works with RC1

--- app.groovy
+++ app.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,10 +27,9 @@ import org.springframework.web.client.HttpClientErrorException
  *
  * @author Greg Turnquist
  */
-@Grab("thymeleaf-spring3")
-@Grab("spring-boot-starter-actuator")
-@Grab("spring-boot-starter-security")
+@Grab("thymeleaf-spring4")
 @Grab("spring-web")
+@Grab("jackson-databind")
 @Controller
 @Log
 class IssueAggregator {
